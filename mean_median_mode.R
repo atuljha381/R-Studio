6515ed8ffdd15e836b758f2d5eca06ed63fd7edc
atuljha381 <mail.atuljha@gail.com>
2020-08-25 20:54:28 +0530

    mean,median,mode in R

--- mean_median_mode.R
+++ mean_median_mode.R
@@ -1,5 +1,18 @@
-var = c(2,3,4,5,6,7,8)
+var = c(2,3,4,9,5,10,7,8)
+var2 = c(1,10,5,8,9)
 
-mean.res <- mean(var, trim = 0)
+mean_res <- mean(var)
+med_res <- median(var2)
 
-print(mean.res)
+mode_res <- function(v){
+  vector1 <- unique(v)
+  vector1[which.max(tabulate(match(v,vector1)))]
+}
+
+var3 <- c(2,1,2,3,1,2,3,4,1,5,5,3,2,3)
+
+cat("\nMean is: ",mean_res)
+
+cat(" \nMedian is: ",med_res)
+
+cat(" \nMode is: ", mode_res(var3))
